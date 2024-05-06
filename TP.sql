008bc0fbaeefee36e30056c875516be28b67a789
Roman Lupiano <romanlupiano@gmail.com>
2024-05-05 23:05:17 -0300

    Planteados los ejercicios adicionales

--- TP.sql
+++ TP.sql
@@ -109,14 +109,64 @@ OFFSET 10;
 -------------------------------------------------------------------------------------------------------
 --Funciones de grupo y uso de GROUP BY y HAVING (esq. Voluntarios)
 --1)Recupere la cantidad mínima, máxima y promedio de horas aportadas por los voluntarios de más de 25 años. 
+SELECT MIN(horas_aportadas), MAX(horas_aportadas), AVG(horas_aportadas)
+FROM voluntario
+WHERE EXTRACT(YEAR FROM AGE(CURRENT_DATE, fecha_nacimiento)) >= 25;
 
 --2)Obtenga la cantidad de voluntarios que tiene cada institución.
+SELECT id_institucion, count(nro_voluntario)
+FROM voluntario
+GROUP BY id_institucion;
 
 --3)Muestre la fecha de nacimiento del voluntario más joven y del más viejo.
+SELECT MIN(fecha_nacimiento), MAX(Fecha_nacimiento)
+FROM voluntario;
 
 --4)Considerando los datos históricos de cada voluntario, indique la cantidad de tareas 
---  distintas que cada uno ha realizado. 
+--  distintas que cada uno ha realizado.
+SELECT nro_voluntario, COUNT(DISTINCT id_tarea)
+FROM historico
+GROUP BY nro_voluntario;
 
 --5)Se quiere conocer los coordinadores que tienen a su cargo menos de 3 voluntarios dentro 
 --  de cada institución.
+SELECT id_coordinador, COUNT(nro_voluntario)
+FROM voluntario
+GROUP BY id_coordinador;
 
+
+-------------------------------------------------------------------------------------------------------
+--Consultas sobre la Base de Datos de Voluntariado: 
+--1)Realice un listado donde, por cada voluntario, se indique las distintas instituciones y tareas 
+--  que ha desarrollado (considere los datos históricos). 
+
+--2)Muestre el apellido, la tarea y las horas aportadas de todos los voluntarios cuyas tareas sean 
+--  de “SA_REP” o “ST_CLERK” y cuyas horas aportadas no sean iguales a 2.500, 3.500 ni 7.000.
+
+--3)Muestre los datos completos de las instituciones que posean director. 
+
+--4)Muestre el apellido e identificador de la tarea de todos los voluntarios que no tienen coordinador.
+
+--5)Muestre el apellido, las horas aportadas y el porcentaje de donación para todos los voluntarios que 
+--  aportan horas (aporte > 0 o distinto de nulo). Ordene los datos de forma descendente según las 
+--  horas aportadas y porcentajes de donaciones.
+
+--6)Liste los identificadores de aquellos coordinadores que coordinan a más de 8 voluntarios.
+
+
+--7)Muestre el identificador de las instituciones y la cantidad de voluntarios que trabajan en 
+--  ellas, sólo de aquellas instituciones que tengan más de 10 voluntarios.
+
+
+
+-------------------------------------------------------------------------------------------------------
+--Consultas sobre la Base de Distribución de Películas:
+--8)Liste los apellidos, nombres y e-mails de los empleados con cuentas de gmail y cuyo sueldo sea 
+--  superior a 1000. 
+
+--9)Muestre los códigos de películas que han recibido entre 3 y 5 entregas. (cantidad de entregas, 
+--  NO cantidad de películas entregadas) 
+
+--10)Liste la cantidad de películas que hay por cada idioma. 
+
+--11)Calcule la cantidad de empleados por departamento. 

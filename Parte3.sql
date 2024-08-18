be06e5a871c44ee2e15eaac1221509cb02f2e45f
RomanLupiano <romanlupiano@gmail.com>
2024-08-18 17:22:00 -0300

    Parte3 Ej13

--- Parte3.sql
+++ Parte3.sql
@@ -1,4 +1,4 @@
---Consultas sobre una tabla (esq. Voluntarios)
+----Consultas sobre una tabla (esq. Voluntarios)
 
 --Consultas con condiciones y ordenamiento
 --1)Muestre el apellido, nombre, las horas aportadas y la fecha de nacimiento de todos los voluntarios cuya tarea
@@ -51,23 +51,56 @@ LIMIT 3;
 
 --Consultas con funciones de grupo, GROUP BY y HAVING 
 --7)Encuentre la cantidad mínima, máxima y promedio de horas aportadas por los voluntarios de más de 30 años.
+SELECT min(horas_aportadas), max(horas_aportadas), avg(horas_aportadas)
+FROM voluntario
+WHERE EXTRACT(YEAR FROM AGE(fecha_nacimiento)) >= 30;
 
 --8)Por cada institución con identificador conocido, indicar la cantidad de voluntarios que trabajan en ella y el total de horas que aportan.
+SELECT id_institucion, count(*), sum(horas_aportadas)
+FROM voluntario
+WHERE id_institucion IS NOT NULL
+GROUP BY id_institucion;
 
 --9)Muestre el identificador de las instituciones y la cantidad de voluntarios que trabajan en ellas, sólo de aquellas instituciones que tengan más de 10 voluntarios.
+SELECT id_institucion, count(*)
+FROM voluntario
+WHERE id_institucion IS NOT NULL
+GROUP BY id_institucion
+HAVING count(*) > 10;
 
 --10)Liste los coordinadores que tienen a su cargo más de 3 voluntarios dentro de una misma institución.
+SELECT id_coordinador, count(*)
+FROM voluntario
+WHERE id_coordinador IS NOT NULL
+GROUP BY id_coordinador
+HAVING count(*) > 3;
 
 -------------------------------------------------------------------------------------------------------
---Consultas sobre más de una tabla (esq. Peliculas): 
+----Consultas sobre más de una tabla (esq. Peliculas): 
 --Consultas para resolver mediante ensamble/s (NATURAL/INNER/OUTER JOIN).
 --11)Muestre los ids, nombres y apellidos de los empleados que no poseen jefe. Incluya también el nombre de la tarea que cada uno realiza,
 --   verificando que el sueldo máximo de la misma sea superior a 14800. 
+SELECT id_empleado, nombre, apellido, nombre_tarea
+FROM empleado e JOIN tarea t
+     ON (e.id_tarea = t.id_tarea)
+WHERE id_jefe IS NULL AND sueldo_maximo > 14800
 
 --12)Determine si hay empleados que reciben un sueldo superior al de sus respectivos jefes.
+SELECT e.id_empleado, e.sueldo, e.id_jefe, j.id_empleado, j.sueldo
+FROM empleado e JOIN empleado j
+     ON (e.id_jefe = j.id_empleado)
+WHERE e.sueldo > j.sueldo
 
 --13)Liste el identificador, nombre y tipo de los distribuidores que hayan entregado películas en idioma Español luego del año 2010. 
 --   Incluya en cada caso la cantidad de películas distintas entregadas.
+SELECT d.id_distribuidor, d.nombre, d.tipo, count(DISTINCT p.titulo)
+FROM distribuidor d
+     JOIN entrega e ON (d.id_distribuidor = e.id_distribuidor)
+     JOIN renglon_entrega r ON (e.nro_entrega = r.nro_entrega)
+     JOIN pelicula p ON (r.codigo_pelicula = p.codigo_pelicula)
+WHERE EXTRACT(YEAR FROM fecha_entrega) > 2010 
+      AND idioma = 'Español'
+GROUP BY d.id_distribuidor
 
 --14)Para cada uno de los empleados registrados en la base, liste su apellido junto con el apellido de su jefe, en caso de tenerlo, 
 --   sino incluya la expresión ‘(no posee)’. Ordene el resultado por el apellido del empleado.
